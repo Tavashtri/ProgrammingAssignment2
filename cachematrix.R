5784932db1eb1a9ecc7349b9bf07ada11e7a565e
Tavashtri <thomas.buffon@gmail.com>
2015-03-17 10:46:14 +0100

    Complete solution
    This is the solution to the pprogramming assignment No 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix takes a matrix as paraameter and
+## stores a it and it's inverse in cache. 
+## It's using a list to set up the four function calls 
+## 1. setMatrix stores the matrix and clears the cache of the inverse (sets it to NULL)
+## 2. getMatrix retrieves the matrix
+## 3. setInverseMatrix solves and stores the inverse of the matrix
+## 4. getInverseMatrix returns the inver matrix.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(matrix = matrix()) {
+    inverseMatrix <- NULL
+    setMatrix <- function(y) {
+        matrix <<- y
+        inverseMatrix <<- NULL
+    }
+    getMatrix <- function() matrix
+    setInverseMatrix <- function(solve) inverseMatrix <<- solve
+    getInverseMatrix <- function() inverseMatrix
+    list(setMatrix = setMatrix, 
+         getMatrix = getMatrix,
+         setInverseMatrix = setInverseMatrix,
+         getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes makeCacheMatrix object as parameter and checks if
+## there exists a caached version of the inverse (That the cache is not NULL)
+## If it finds a value it returns it. Otherwise it calculates the inverse,
+## Stores it in coche through makeCacheMatrix and finally returns it.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrix, ...) {
+    inverseMatrix <- matrix$getInverseMatrix()
+    if(!is.null(inverseMatrix)) {
+        message("getting cached data")
+        return(inverseMatrix)
+    }
+    data <- matrix$getMatrix()
+    inverseMatrix <- solve(data, ...)
+    matrix$setInverseMatrix(inverseMatrix)
+    inverseMatrix
 }
